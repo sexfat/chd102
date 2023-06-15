2d6871665c636d6229974f789a84a8cafa8be5e9
sexfat <bryant6831@gmail.com>
2023-06-15 11:46:39 +0800

    mv

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,21 @@ const {
 } = require('gulp');
 
 // function 任務
-
 function defaultTask(cb){
   console.log('gulp ok') 
   cb();
 } 
 
 
-exports.df = defaultTask
+exports.log = defaultTask
+
+
+// 檔案搬家任務
+function move(){
+   return src('index.html').pipe(dest('dist/'))
+}
+
+exports.mv = move;
+
+
 
