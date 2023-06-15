c793790f5e0d0fcf82f0d4a8e9f0515bcf35d405
sexfat <bryant6831@gmail.com>
2023-06-15 12:01:01 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -25,3 +25,27 @@ exports.mv = move;
 
 
 
+// 任務的順序
+function TaskA(cb){
+    console.log('任務A') 
+    cb();
+  } 
+
+
+function TaskB(cb){
+    console.log('任務B') 
+    cb();
+  }
+
+exports.async = series(TaskA , TaskB);//有順序的執行任務
+exports.sync = parallel(TaskA , TaskB);//一起執行任務
+
+
+  
+  
+
+  
+
+
+
+
