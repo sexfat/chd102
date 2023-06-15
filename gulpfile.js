0cc41bbc225cb6880adeed24ef3f672a8241ca27
sexfat <bryant6831@gmail.com>
2023-06-15 12:04:59 +0800

    all

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,18 @@ const {
 } = require('gulp');
 
 // function 任務
-function defaultTask(cb){
-  console.log('gulp ok') 
-  cb();
-} 
+function defaultTask(cb) {
+    console.log('gulp ok')
+    cb();
+}
 
 
 exports.log = defaultTask
 
 
 // 檔案搬家任務
-function move(){
-   return src('index.html').pipe(dest('dist/'))
+function move() {
+    return src('index.html').pipe(dest('dist/'))
 }
 
 exports.mv = move;
@@ -26,25 +26,42 @@ exports.mv = move;
 
 
 // 任務的順序
-function TaskA(cb){
-    console.log('任務A') 
+function TaskA(cb) {
+    console.log('任務A')
     cb();
-  } 
+}
 
 
-function TaskB(cb){
-    console.log('任務B') 
+function TaskB(cb) {
+    console.log('任務B')
     cb();
-  }
+}
 
-exports.async = series(TaskA , TaskB);//有順序的執行任務
-exports.sync = parallel(TaskA , TaskB);//一起執行任務
+function TaskC(cb) {
+    console.log('任務C')
+    cb();
+}
+
+function TaskD(cb) {
+    console.log('任務D')
+    cb();
+}
+
+function TaskE(cb) {
+    console.log('任務E')
+    cb();
+}
+
+exports.async = series(TaskA, TaskB);//有順序的執行任務
+exports.sync = parallel(TaskA, TaskB);//一起執行任務
+
+exports.all = series(TaskA , TaskB , parallel(TaskC , TaskD) , TaskE);
 
 
-  
-  
 
-  
+
+
+
 
 
 
